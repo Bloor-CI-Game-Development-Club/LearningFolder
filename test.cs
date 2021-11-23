44d039e41a8dd0152b2c6477375beded9ac96fb5
James <66389450+Jam-Cai@users.noreply.github.com>
2021-11-23 17:13:49 -0500

    Example commit message

---
+++ test.cs
@@ -0,0 +1,12 @@
+using System; // Uses classes and functions from the System Namespace(e.g. Console,
+namespace HelloWorld // A container for classes and namespaces, similar to System
+{
+  class Program 
+  {
+    static void Main(string[] args) // method where  your code will run
+    {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}
+
