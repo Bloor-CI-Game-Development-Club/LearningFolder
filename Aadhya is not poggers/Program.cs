3aa153a753a33b6b145854fcf7915f4f9c0c862e
AntimonySB2468 <69812034+ambiguous-cipher@users.noreply.github.com>
2021-11-28 17:56:18 -0500

    Made a new file
    Made a new file

---
+++ Aadhya is not poggers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aadhya_is_not_poggers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oh wow look, self-deprication! I think my computer runs on potatoes at this point...");
+        }
+    }
+}
