53f163bf67425849178e1fd534c5db9efc6e5c74
JackInTheBack <93162572+JackInTheBack@users.noreply.github.com>
2021-11-28 17:30:26 -0500

    made references in code
    the legend never dies

---
+++ I came here to be Jack and say no more than twelve/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace I_came_here_to_be_Jack_and_say_no_more_than_twelve
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dread it, run from it, giantdad still arrives.*CLANG* ohno");
+        }
+    }
+}
